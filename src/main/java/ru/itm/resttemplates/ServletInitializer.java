2b90d754a066aa7a80901497aaaaa3d33f78aceb
Olga Ivlieva <sunchess55@gmail.com>
2023-08-27 22:36:39 +0300

    first commit

---
+++ src/main/java/ru/itm/resttemplates/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ru.itm.resttemplates;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestTemplatesApplication.class);
+	}
+
+}
---
+++ src/test/java/ru/itm/resttemplates/RestTemplatesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itm.resttemplates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
